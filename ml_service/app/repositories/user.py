b869e5a04dd297e5d3d0f5b0303f2c0a7367a3f8
davoodq12w <davod.q12w@gmail.com>
2026-08-28 23:11:11 +0330

    config database for fastapi

---
+++ ml_service/app/repositories/user.py
@@ -0,0 +1,31 @@
+from sqlalchemy import text
+from typing import Optional
+from sqlalchemy.ext.asyncio import AsyncSession
+from ..database.session import get_session
+from fastapi import Depends
+
+
+class UserRepository:
+    def __init__(self, session: AsyncSession):
+        self.session = session
+
+    async def get_user_basic(self, user_id: int) -> Optional[dict]:
+        """
+        فقط فیلدهای مورد نیاز برای feature engineering:
+        - user_id
+        - created
+        """
+        query = text("""
+                     SELECT id AS user_id, created
+                     FROM account_filmbazuser
+                     WHERE id = :user_id
+                     """)
+
+        result = await self.session.execute(query, {"user_id": user_id})
+        row = result.mappings().first()
+
+        return dict(row) if row else None
+
+
+def get_user_repository(session: AsyncSession = Depends(get_session)):
+    return UserRepository(session)
