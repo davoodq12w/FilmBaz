67f9652e0ca4c44b91f3d8de5406075c8e0f0fcb
davoodq12w <davod.q12w@gmail.com>
2026-08-31 18:52:39 +0330

    add build_model api

--- ml_service/app/api/recommendation.py
+++ ml_service/app/api/recommendation.py
@@ -1,6 +1,8 @@
-from fastapi import APIRouter, Depends, HTTPException
-from app.services.raw_data import row_data, RowData
+from fastapi import APIRouter, Depends
+from ..services.raw_data import row_data, RowData
 from pydantic import BaseModel
+from ..preprocessing.pipeline import get_processed_dataset
+from ..training.trainer import train_model
 
 router = APIRouter()
 
@@ -14,3 +16,11 @@ class DataInput(BaseModel):
 async def get_recommendation(data: DataInput, service: RowData = Depends(row_data)):
     result = await service.get_raw_data(user_id=data.user_id, movie_ids=data.movie_ids)
     return result
+
+
+@router.get("/build_model/")
+def build_model():
+    get_processed_dataset()
+    train_model()
+
+
