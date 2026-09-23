b93bf4d90d98ca16284a7a53ce00c1701e9592bd
davoodq12w <davod.q12w@gmail.com>
2026-09-23 12:03:47 +0330

    comments for analytics api

--- analytics/api/serializers.py
+++ analytics/api/serializers.py
@@ -3,6 +3,9 @@ from analytics.models import Interaction
 
 
 class InteractionSerializer(serializers.Serializer):
+    """
+    Seriailzer for create Interaction object
+    """
     movie_id = serializers.IntegerField(read_only=True)
     movie_slug = serializers.SlugField(required=True)
     interaction_type = serializers.ChoiceField(
--- analytics/api/views.py
+++ analytics/api/views.py
@@ -7,7 +7,12 @@ from api_template import FilmBazAPI
 from film.models import Movie
 from analytics.models import Interaction
 
+
 class IntractionApi(FilmBazAPI):
+    """
+    Api View user for creation of interactions
+    only authenticated users access the API
+    """
 
     @extend_schema(
         description="ساختن اینترکشن برای کاربر",
@@ -25,7 +30,13 @@ class IntractionApi(FilmBazAPI):
         tags=["interaction"],
     )
     def post(self, request: Request, *args, **kwargs):
+        """
+        create interactions
+        take id & slug of movie and create interaction object for authenticated user.
+        """
         serializer = InteractionSerializer(data=request.data)
+
+        # if there was any validations error raise automaticlly
         serializer.is_valid(raise_exception=True)
 
         movie_id = serializer.validated_data['movie_id']
@@ -37,6 +48,7 @@ class IntractionApi(FilmBazAPI):
         except Movie.DoesNotExist:
             return Response({"Error": "movie with this data not exsits."}, status=status.HTTP_404_NOT_FOUND)
 
+        # enumerate of interactions weight for simpl usage.
         weights = {
             "view": 0.2,
             "like": 1.0,
@@ -48,6 +60,8 @@ class IntractionApi(FilmBazAPI):
             "complete": 1.2,
         }
         interaction_type = serializer.validated_data['interaction_type']
+
+        # interactions that those count are importent
         if interaction_type in ["view", "comment", "search"]:
             Interaction.objects.create(
                 user=request.user,
@@ -55,6 +69,7 @@ class IntractionApi(FilmBazAPI):
                 interaction_type=interaction_type,
                 weight=weights[interaction_type],
             )
+        # interactions that those count are not importent
         elif interaction_type in ["like", "save", "share", "watch", "complete"]:
             Interaction.objects.get_or_create(
                 user=request.user,
@@ -62,4 +77,6 @@ class IntractionApi(FilmBazAPI):
                 interaction_type=interaction_type,
                 weight=weights[interaction_type],
             )
+
+        # don't need to return interaction object
         return Response({"Success": "Interaction created successfully."}, status=status.HTTP_201_CREATED)
